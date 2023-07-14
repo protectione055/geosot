2714c3bdd360fc5f25d058e99cb55dbdfadb3b70
Ziming Zhang <protectione055@foxmail.com>
2023-07-14 21:30:25 +0800

    Implemented a Morton encoding/decoding through bitwise interleaving

--- build.sbt
+++ build.sbt
@@ -4,10 +4,10 @@ ThisBuild / scalaVersion := "2.13.11"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "geosot"
+    name := "geosot",
+    scalacOptions += "-Yrangepos"
   )
 
-libraryDependencies += Seq(
-  "com.lihaoyi" %% "os-lib" % "0.9.1",
-  "com.lihaoyi" %% "geny" % "1.0.0"
-)
+libraryDependencies += "com.lihaoyi" %% "os-lib" % "0.9.1"
+libraryDependencies += "com.lihaoyi" %% "geny" % "1.0.0"
+libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,24 @@
 import scala.io._
 import os._
 import geny.Generator
+import geosot.MortonCode
 
 object Main {
   def main(args: Array[String]): Unit = {
     val proj_dir = os.pwd
     val input_files: Seq[os.Path] = os.list(proj_dir / "dataset").tail
-    for(file <- input_files) {
+//    readTestDataFromFile(input_files)
+
+    val morton_coder = MortonCode
+    val coordinate: Array[Int] = Array[Int](-1, -1, -1)
+    val obtained = morton_coder.enCode(coordinate)
+    val expected = Array[Byte](-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1)
+  }
+
+  private def readTestDataFromFile(input_files: Seq[os.Path]): Unit = {
+    for (file <- input_files) {
       val lines: Seq[String] = os.read.lines(file)
-      for(line <- lines) {
+      for (line <- lines) {
         var cells: Seq[String] = line.split(", ")
         val lat: String = cells(0)
         val lon: String = cells(1)
@@ -17,4 +27,5 @@ object Main {
       }
     }
   }
+
 }
