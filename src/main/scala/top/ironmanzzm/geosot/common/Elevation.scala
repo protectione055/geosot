2d0528cf48fe5c158cee22bd771982aa65e47597
Ziming Zhang <protectione055@foxmail.com>
2023-07-31 12:03:21 +0800

    feat(geosot): 完成高度域方向上的剖分编码实现

---
+++ src/main/scala/top/ironmanzzm/geosot/common/Elevation.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
+package top.ironmanzzm.geosot.common
+
+/**
+ * GeoSOT中高程的封装类。
+ * 设定高度单位是度分秒，参考椭球参数，建立空间高度单位与千米、米的换算关系：D˚×πR/180˚=H km。
+ * 其中，R为地球平均半径，H为地心与该经纬度平面的高程。
+ *
+ * @author Ziming Zhang
+ */
+class Elevation extends Dimension {
+    private final val EARTH_RADIUS = 6302.106722602182
+
+    override def getValue(precision: Int): Int = 0
+}
+
+object Elevation {
+    /**
+     *
+     * @param elevation
+     * @return
+     */
+    def apply(elevation: String) = {
+        ???
+    }
+}
